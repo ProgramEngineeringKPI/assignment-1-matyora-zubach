3f62fbe04770062e389ab0b25cc39e1373687500
Yurwar <yura.matora11@gmail.com>
2018-04-10 21:53:16 +0300

    Add comments and sort results array

--- main.cpp
+++ main.cpp
@@ -1,44 +1,73 @@
 #include <string>
-#include <vector>
 #include <iostream>
 #include <cstring>
 
 using namespace std;
+
+void insertionSort(int *, string*, int);
+void swap(int &, int &);
 
 int main() {
     FILE *pFile = fopen("premier_league.csv", "r");
     if(pFile == nullptr) {
         cout << "Error";
     } else {
-        auto *number = new char;
-        fgets(number, sizeof(number), pFile);
-        int amountOfTeams = atoi(number);
-        auto* results = new int[amountOfTeams];
+        auto *number = new char;             //
+        fgets(number, sizeof(number), pFile);// Take the first number from file
+        int amountOfTeams = atoi(number);    //
 
-        char unparsedData[amountOfTeams][100];
-        for(int i = 0; i < amountOfTeams; i++) {
-            fgets(unparsedData[i], 100, pFile);
-            //printf("%s", unparsedData[i]);
+        auto* resultPoints = new int[amountOfTeams];//
+        string resultTeams[amountOfTeams];          //Arrays of results
+
+        char unparsedData[amountOfTeams][100];        //Read from file
+        for(int i = 0; i < amountOfTeams; i++) {      //
+            fgets(unparsedData[i], 100, pFile);       //
         }
+        fclose(pFile);
 
-        string parsedData[amountOfTeams][11];
-
-        for(int i = 0; i < amountOfTeams; i++) {
+        string parsedData[amountOfTeams][11];         //Change for convenient format
+        for(int i = 0; i < amountOfTeams; i++) {      //by split for delimiter ","
             char *pch = strtok(unparsedData[i], ",");
             for(int j = 0; j < 11; j++) {
                 parsedData[i][j] = pch;
                 pch = strtok(nullptr, ",");
-                //cout << parsedData[i][j];
             }
         }
-
+        //Counting final results
         for(int i = 0; i < amountOfTeams; i++) {
             for(int j = 1; j < 11; j++) {
-                if(parsedData[i][j][0] > parsedData[i][j][2]) results[i] += 3;
-                if(parsedData[i][j][0] == parsedData[i][j][2]) results[i] += 1;
+                if(parsedData[i][j][0] > parsedData[i][j][2]) resultPoints[i] += 3;
+                if(parsedData[i][j][0] == parsedData[i][j][2]) resultPoints[i] += 1;
             }
-            cout << results[i] << ',';
         }
-        cout << '\n';
+        for(int i = 0; i < amountOfTeams; i++) {
+            resultTeams[i] = parsedData[i][0];
+        }
+
+        insertionSort(resultPoints, resultTeams, amountOfTeams); //Sort two result arrays
+
+        //Checkout for arr
+        for(int i = 0; i < amountOfTeams; i++) {
+            cout << resultTeams[i] << ':' << resultPoints[i] << '\n';
+        }
+        //
     }
+}
+
+void insertionSort(int* arrResults, string* arrTeams, int arrLength) {
+    int i, j, k;
+    k = 0;
+    for(i = 0; i < arrLength; i++) {
+        j = i;
+        while(j > k && arrResults[j] > arrResults[j - 1]) {
+            swap(arrResults[j], arrResults[j - 1]);
+            swap(arrTeams[j], arrTeams[j - 1]);
+            j--;
+        }
+    }
+}
+
+void swap(int &a, int &b) {
+    int tmp;
+    tmp = a; a = b; b = tmp;
 }
